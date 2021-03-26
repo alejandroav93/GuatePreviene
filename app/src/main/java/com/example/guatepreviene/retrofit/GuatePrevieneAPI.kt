9f2078cfbe5fd5c104964985430f1221d4a8fff3
Alejandro Alvarez V <59927580+alejandroav93@users.noreply.github.com>
2021-03-25 20:03:32 -0600

    Updated Graddle versions

--- app/src/main/java/com/example/guatepreviene/retrofit/GuatePrevieneAPI.kt
+++ app/src/main/java/com/example/guatepreviene/retrofit/GuatePrevieneAPI.kt
@@ -6,6 +6,16 @@ import retrofit2.http.GET
 import retrofit2.http.Path
 
 interface GuatePrevieneAPI {
-    @GET("pokemon/{id}")
-    fun getPokemonById(@Path("id") id: String?): Call<JsonObject?>?
+
+    @GET("information")
+    fun getEmergencies(): Call<JsonObject?>?
+
+    @GET("covid")
+    fun getStatistics(): Call<JsonObject?>?
+
+    @GET("news")
+    fun getNews(): Call<List<JsonObject?>?>?
+
+    @GET("centres")
+    fun getMaps(): Call<List<JsonObject?>?>?
 }
