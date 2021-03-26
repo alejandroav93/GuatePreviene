514c1f7609ef1ef9fd9bc6977ae39df60dc823ba
Alejandro Alvarez V <59927580+alejandroav93@users.noreply.github.com>
2021-03-25 20:55:01 -0600

    Added Recycled retrofit function for API consumption

--- app/src/main/java/com/example/guatepreviene/retrofit/Utils.kt
+++ app/src/main/java/com/example/guatepreviene/retrofit/Utils.kt
@@ -2,6 +2,6 @@ package com.example.guatepreviene.retrofit
 
 object Utils {
 
-    var URL_BASE = "http://gtpreviene.researchmobile.co:3000/api/covid"
+    var URL_BASE = "http://gtpreviene.researchmobile.co:3000/api/"
 
 }
--- app/src/main/java/com/example/guatepreviene/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/guatepreviene/ui/home/HomeFragment.kt
@@ -1,6 +1,7 @@
 package com.example.guatepreviene.ui.home
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -8,7 +9,15 @@ import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import com.example.guatepreviene.BuildConfig
 import com.example.guatepreviene.R
+import com.example.guatepreviene.retrofit.Retrofit2
+import com.google.gson.JsonObject
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import java.util.*
+import kotlinx.android.synthetic.main.*
 
 class HomeFragment : Fragment() {
 
@@ -18,14 +27,36 @@ class HomeFragment : Fragment() {
             inflater: LayoutInflater,
             container: ViewGroup?,
             savedInstanceState: Bundle?
+
     ): View? {
-        homeViewModel =
-                ViewModelProvider(this).get(HomeViewModel::class.java)
-        val root = inflater.inflate(R.layout.fragment_home, container, false)
-        val textView: TextView = root.findViewById(R.id.text_home)
-        homeViewModel.text.observe(viewLifecycleOwner, Observer {
-            textView.text = it
+
+        val retrofit2 = Retrofit2()
+        retrofit2.service.getEmergencies()?.enqueue(object :
+            Callback<JsonObject?> {
+            override fun onResponse(call: Call<JsonObject?>, response: Response<JsonObject?>) {
+                if (response.isSuccessful) {
+                    val data: JsonObject? = response.body()
+                    if (BuildConfig.DEBUG && data == null) {
+                        error("Assertion failed")
+                    }
+                    val print = ("\n\nHabilidad: " +
+                            data!!.getAsJsonArray("abilities")[1].asJsonObject
+                                .getAsJsonObject("ability")["name"].toString().toUpperCase(Locale.ROOT)
+                            + "\nID: " + data["id"]
+                            + "\nNombre: " + data["name"].toString().toUpperCase(Locale.ROOT)
+                            + "\nExperiencia Base: " + data["base_experience"] +
+                            "\nAltura: " + data["height"] + "0 cm"
+                            + "\nPeso: " + data["weight"] + " gramos")
+                    pokemon.text = print
+                    Log.i("Pokemon", data.toString())
+                } else {
+                    Log.e("Error", "Hubo un error inesperado!")
+                }
+            }
+
+            override fun onFailure(call: Call<JsonObject?>, t: Throwable) {
+                Log.e("Error", t.toString())
+            }
         })
-        return root
     }
 }
